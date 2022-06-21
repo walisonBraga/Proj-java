a1da7baf6566d77f8199506323e1ee3ab76f6f96
walisonBraga <walison.lemos@gmail.com>
2022-06-21 10:35:45 -0500

    Update Cliente.js

--- Cliente.js
+++ Cliente.js
@@ -1,4 +1,13 @@
-class cliente {
+export class cliente {
     nome;
-    cpf;
+    _cpf;
+
+    get cpf(){
+       return this._cpf;
+    }
+
+    constructor(nome, cpf){
+        this.nome = nome;
+        this._cpf= cpf;
+    }
 }
