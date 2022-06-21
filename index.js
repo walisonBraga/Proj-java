eedafd75ceb6b505b24d649b789c609a67d7b6bb
walisonBraga <walison.lemos@gmail.com>
2022-06-21 07:42:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-class cliente {
-    nome;
-    cpf;
-}
-
-class contaCorrente{
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-        this._saldo += valor;
-    }
-
-}
-
 const cliente1 = new cliente();
 cliente1.nome = "Walison";
 cliente1.cpf = 23578965469;
