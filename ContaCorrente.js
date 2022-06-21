7533d4a4173c3833375456dcacf042aa9b9fc651
walisonBraga <walison.lemos@gmail.com>
2022-06-21 10:35:49 -0500

    Update ContaCorrente.js

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,6 +1,30 @@
-class contaCorrente{
+import { cliente } from "./Cliente.js";
+
+export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
+    _cliente;
     _saldo = 0;
+
+    set cliente(novoValor){
+        if(novoValor instanceof cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
@@ -13,6 +37,13 @@ class contaCorrente{
             return;
         }
         this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        conta.cidade = "São Paulo";
+        valor = 20;
     }
 
 }
