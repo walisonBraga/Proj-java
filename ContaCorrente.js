fcefde909b754eaf983ec3c4f0816ca004fef4df
walisonBraga <walison.lemos@gmail.com>
2022-06-21 07:41:39 -0500

    Create ContaCorrente.js

---
+++ ContaCorrente.js
@@ -0,0 +1,18 @@
+class contaCorrente{
+    agencia;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+        this._saldo += valor;
+    }
+
+}
